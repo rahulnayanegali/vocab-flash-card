408c8a70dd4d744c25c13295bc1abbeb87a77022
Rahul Nayanegali <rahulnayanegali@Rahuls-MacBook-Pro.local>
2022-04-27 01:45:07 +0530

    fix: firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDWpMKuyGKOeO9lIN2nQJkBngtH31Mnvso",
-    authDomain: "vocab-flash-card-40223.firebaseapp.com",
-    projectId: "vocab-flash-card-40223",
-    storageBucket: "vocab-flash-card-40223.appspot.com",
-    messagingSenderId: "148517469668",
-    appId: "1:148517469668:web:73cd8ec29c37c544631bf1",
-    measurementId: "G-GHBD93VKFQ"
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId",
+    measurementId: "measurementId"
   };
 
 // Initialize Firebase and Firestore
